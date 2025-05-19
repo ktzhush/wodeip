ddc672a88156e4c7dce690f7091330b883769f11
ktzhush <ktzhush@gmail.com>
2025-05-19 10:20:05 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,10 +4,11 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
+#urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
         'https://ip.164746.xyz'
         ]
-
+urls = ['https://cf.090227.xyz/'
+        ]
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
@@ -27,7 +28,7 @@ with open('ip.txt', 'w') as file:
         # 根据网站的不同结构找到包含IP地址的元素
         if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
             elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
+        elif url == 'https://cf.090227.xyz':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
